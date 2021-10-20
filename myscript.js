3d8b8b41a8b98524821371b058de82e4373cb790
JackSparring420 <emanuele.arconte94@gmail.com>
2021-10-20 16:13:23 +0200

    exercise completed

--- myscript.js
+++ myscript.js
@@ -7,7 +7,9 @@
 let name = prompt('Come ti chiami?');
 let age = prompt('Quanti anni hai?');
 let favoriteColor = prompt('Qual è il tuo colore preferito?');
+let final_number = 21
+let password = name + age + favoriteColor + final_number
 
 document.getElementById('password_assigned_a').innerHTML = "La password assegnata &#232: " + name + age + favoriteColor + "21";
 
-document.getElementById('password_assigned_b').innerHTML = `La password assegnata &#232: ${name}${age}${favoriteColor}21`;
+document.getElementById('password_assigned_b').innerHTML = `La password assegnata &#232: ${password}`;
