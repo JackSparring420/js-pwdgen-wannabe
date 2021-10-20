e3aa52c00dc710b0a78172f3b02b0fab601e0d30
JackSparring420 <emanuele.arconte94@gmail.com>
2021-10-20 15:12:34 +0200

    exercise completed

---
+++ myscript.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21
+
+
+let name = prompt('Come ti chiami?');
+let age = prompt('Quanti anni hai?');
+let favoriteColor = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById('password_assigned_a').innerHTML = "La password assegnata &#232: " + name + age + favoriteColor + "21";
+
+document.getElementById('password_assigned_b').innerHTML = `La password assegnata &#232: ${name}${age}${favoriteColor}21`;
